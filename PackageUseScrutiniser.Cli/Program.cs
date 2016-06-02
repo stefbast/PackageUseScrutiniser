b14370d141127b5e03586e1fce3a3512ab1bd234
Stefaan Bastin <stefaan.bastin@gmail.com>
2016-06-02 13:21:36 +0200

    list all packages in folder

--- PackageUseScrutiniser.Cli/Program.cs
+++ PackageUseScrutiniser.Cli/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
+using System.Xml.Linq;
 
 namespace PackageUseScrutiniser.Cli
 {
@@ -9,6 +9,21 @@ namespace PackageUseScrutiniser.Cli
     {
         static void Main(string[] args)
         {
+            var packagesConfigs = Directory.EnumerateFiles(@"C:\_Source\IAM\FIN-Genesis\AMGStocks\Development", "packages.config", SearchOption.AllDirectories);
+            foreach (var packageConfig in packagesConfigs)
+            {
+                XDocument doc = XDocument.Load(packageConfig);
+                var packages = doc.Root
+                                  .Elements("package")
+                                  .Where(package => package.Attribute("id").Value == "Gns.Sly.LocalStock.Contracts")
+                                  .ToList();
+                if (packages.Count > 0)
+                {
+                    Console.WriteLine(packageConfig);    
+                }
+                
+            }
+
         }
     }
 }
