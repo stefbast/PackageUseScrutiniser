0bfbd5788d9afb78d92c78318968b2db3233d6af
Stefaan Bastin <stefaan.bastin@gmail.com>
2016-06-02 15:48:04 +0200

    refactor cli to get aguments

--- PackageUseScrutiniser.Cli/Program.cs
+++ PackageUseScrutiniser.Cli/Program.cs
@@ -5,34 +5,50 @@ using PackageUseScrutiniser.Core;
 
 namespace PackageUseScrutiniser.Cli
 {
-    class Program
+    internal static class Program
     {
         static void Main(string[] args)
         {
-            
-            IEnumerable<string> packages = new List<string>();
-            var packageFinder = new PackageFinder();
-            switch (args.Length)
-            {                
-                case 1:
-                    if (args[0] == "--help")
-                    {
-                        PrintHelp();
-                    }
-                    packages = packageFinder.GetPackages(Directory.GetCurrentDirectory(), args[0]);
-                    break;
-                case 2:
-                    packages = packageFinder.GetPackages(args[0], args[1]);
-                    break;
-                default:
-                    PrintHelp();
-                    break;
+            if (args.Length == 0 || args[0] == "--help" || args.Length > 2)
+            {
+                PrintHelp();
+                return;
+            }
+       
+            var path = GetPath(args);
+            if (!Directory.Exists(path))
+            {
+                Console.WriteLine("Path {0} does not exist", path);
+                return;
             }
 
-            foreach (var package in packages)
+            var packageId = GetPackageId(args);            
+            var packageFinder = new PackageFinder();
+            
+            foreach (var package in packageFinder.GetPackages(packageId, path))
             {
                 Console.WriteLine(package);
             }
+        }
+
+        private static string GetPath(string[] args)
+        {
+            if (args.Length >= 2)
+            {
+                return args[1];
+            }
+            
+            return Directory.GetCurrentDirectory();                        
+        }
+
+        private static string GetPackageId(IList<string> args)
+        {
+            if (args.Count >= 1)
+            {
+                return args[0];
+            }
+
+            return null;
         }
 
         private static void PrintHelp()
